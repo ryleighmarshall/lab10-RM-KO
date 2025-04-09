35cdd8b30e231c97bece9f99b097ce5ab3459a28
Kalista Oberes <kalistafaithg@gmail.com>
2025-04-09 10:23:52 -0400

    Added all operations to calculator.py

--- calculator.py
+++ calculator.py
@@ -7,24 +7,25 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b / a
 
-def log(a, b):
-    if a <= 0 or b <= 0:
+def logarithm(a, b):
+    if b <= 0 or a <= 0:
         raise ValueError
     return math.log(a, b)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
