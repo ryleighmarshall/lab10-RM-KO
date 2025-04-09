fe94dd9c5fbb42996ee9775a975ed3ff025895ee
Home <ryleighmarshall@ufl.edu>
2025-04-09 10:21:50 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -11,25 +11,25 @@ import math
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     else:
         return b/a
 
-def logarithm(a, b):
-    if b == 0:
+def log(a, b):
+    if b <= 0 or a <= 0:
         raise ValueError
     else:
         return math.log(a,b)
 
-def exponent(a, b):
+def exp(a, b):
     return a**b
 
 
